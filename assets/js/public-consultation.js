14526239a1022678025d4aae1ab50ad0bdf4c625
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-07-05 17:39:25 -0300

    fix: diminuição da quantidade de requisições ao realizar a pesquisa

--- assets/js/public-consultation.js
+++ assets/js/public-consultation.js
@@ -1,3 +1,41 @@
+const publicConsultation = {
+    researchText: '',
+    mountSearchResult(searchResult) {
+        const html = searchResult.map(value => {
+            return `
+                <article class="objeto clearfix" id="public-consultation-wrapper">
+                    <h1>
+                        <a href="">
+                            ${value.title}
+                        </a>
+                    </h1>
+                    <div class="objeto-meta">
+                        <span class="label">
+                            ${value.subtitle}
+                        </span>
+                    </div>
+                    <div class="objeto-meta">
+                        <span class="label" style="word-wrap: break-word;">
+                            <a href="${value.google_docs_link}" target="_blank">
+                                ${value.google_docs_link}
+                            </a>
+                        </span>
+                    </div>
+                    <div class="entity-actions">
+                        <a class="btn btn-small btn-primary" href="${MapasCulturais.createUrl('consulta-publica', 'edit', { 'id': value.id })}">
+                            editar
+                        </a>
+                        <button class="btn btn-small btn-danger" del-public-consultation-btn data-public-consultation-id="${value.id}">
+                            excluir
+                        </button>
+                    </div>
+                </article>`
+        }).join('')
+
+        return html
+    }
+}
+
 $(() => {
     $('#create-public-consultation-form, #edit-public-consultation-form').on('submit', event => {
         event.preventDefault()
@@ -62,57 +100,75 @@ $(() => {
         })
     })
 
-    $('[search-input]').on('keyup', (event) => {
-        const keyCode = event.keyCode
-
+    $('[search-input]').on('keyup', function (event) {
         // Ignora algumas teclas (tab, shift, ctrl) para que não seja feita requisição ao clicá-las
-        if ((keyCode >= 9 && keyCode <= 45) || (keyCode >= 91 && keyCode <= 93) || (keyCode >= 112 && keyCode <= 145)) return
+        const keyCode = event.keyCode
+        if (event.ctrlKey || (keyCode >= 9 && keyCode <= 45) || (keyCode >= 91 && keyCode <= 93) || (keyCode >= 112 && keyCode <= 145)) return
 
-        const status = parseInt(event.currentTarget.dataset.status)
-        const statusLabel = status === 1 ? 'published' : 'unpublished'
-        const text = event.currentTarget.value
+        $(this).each(function () {
+            $(this).data('events', $.extend(true, {}, $._data(this, 'events')))
+        })
+        $(this).off()
 
-        if (text.length) {
-            $.ajax({
-                type: "GET",
-                url: MapasCulturais.createUrl('consulta-publica', 'search'),
-                data: {
-                    status,
-                    text,
-                },
-                dataType: "json",
-                success(res) {
-                    console.log('requisição')
-                    const notFoundElement = '<h4 style="text-align: center; margin: 40px 0 0;">Pesquisa não encontrada</h4>'
-                    const searchHtml = res.length ? mountSearchResult(res) : notFoundElement
+        $('#spinner-search').removeClass('d-none')
 
-                    $(`#${statusLabel}-wrapper`).empty()
-                    $(`#${statusLabel}-wrapper`).html(searchHtml)
-                },
-                error() {
-                    // 
-                }
+        // Espera 1 segundo após a digitação para realizar a requisição
+        setTimeout(() => {
+            const status = parseInt(event.currentTarget.dataset.status)
+            const statusLabel = status === 1 ? 'published' : 'unpublished'
+            publicConsultation.researchText = $(this).val()
+
+            if (publicConsultation.researchText.length) {
+                $.ajax({
+                    type: "GET",
+                    url: MapasCulturais.createUrl('consulta-publica', 'search'),
+                    data: {
+                        status,
+                        text: publicConsultation.researchText,
+                    },
+                    dataType: "json",
+                    success(res) {
+                        const notFoundElement = '<h4 style="text-align: center; margin: 40px 0 0;">Pesquisa não encontrada</h4>'
+                        const searchHtml = res.length ? publicConsultation.mountSearchResult(res) : notFoundElement
+
+                        $(`#${statusLabel}-wrapper`).empty()
+                        $(`#${statusLabel}-wrapper`).html(searchHtml)
+
+                        $('#spinner-search').addClass('d-none')
+                    },
+                    error() {
+                        // 
+                    }
+                })
+            } else {
+                $.ajax({
+                    type: "GET",
+                    url: MapasCulturais.createUrl('consulta-publica', 'allByStatus'),
+                    data: {
+                        status,
+                    },
+                    dataType: "json",
+                    success(res) {
+                        const searchHtml = publicConsultation.mountSearchResult(res)
+
+                        $(`#${statusLabel}-wrapper`).empty()
+                        $(`#${statusLabel}-wrapper`).html(searchHtml)
+
+                        $('#spinner-search').addClass('d-none')
+                    },
+                    error() {
+                        // 
+                    }
+                })
+            }
+
+            $(this).each(function () {
+                let $self = $(this)
+                $.each($(this).data('events'), function (_, e) {
+                    $self.on(e[0].type, e[0].handler)
+                })
             })
-        } else {
-            $.ajax({
-                type: "GET",
-                url: MapasCulturais.createUrl('consulta-publica', 'allByStatus'),
-                data: {
-                    status,
-                },
-                dataType: "json",
-                success(res) {
-                    console.log('requisição')
-                    const searchHtml = mountSearchResult(res)
-
-                    $(`#${statusLabel}-wrapper`).empty()
-                    $(`#${statusLabel}-wrapper`).html(searchHtml)
-                },
-                error() {
-                    // 
-                }
-            })
-        }
+        }, 1000)
     })
 })
 
@@ -158,39 +214,4 @@ const getData = (event) => {
     }
 
     return data
-}
-
-const mountSearchResult = (searchResult) => {
-    const html = searchResult.map(value => {
-        return `
-            <article class="objeto clearfix" id="public-consultation-wrapper">
-                <h1>
-                    <a href="">
-                        ${value.title}
-                    </a>
-                </h1>
-                <div class="objeto-meta">
-                    <span class="label">
-                        ${value.subtitle}
-                    </span>
-                </div>
-                <div class="objeto-meta">
-                    <span class="label" style="word-wrap: break-word;">
-                        <a href="${value.google_docs_link}" target="_blank">
-                            ${value.google_docs_link}
-                        </a>
-                    </span>
-                </div>
-                <div class="entity-actions">
-                    <a class="btn btn-small btn-primary" href="${MapasCulturais.createUrl('consulta-publica', 'edit', { 'id': value.id })}">
-                        editar
-                    </a>
-                    <button class="btn btn-small btn-danger" del-public-consultation-btn data-public-consultation-id="${value.id}">
-                        excluir
-                    </button>
-                </div>
-            </article>`
-    }).join('')
-
-    return html
 }
--- layouts/parts/consulta-publica/search-input.php
+++ layouts/parts/consulta-publica/search-input.php
@@ -1,3 +1,8 @@
-<div style="display: flex; justify-content: flex-end;">
-    <input type="text" name="search-input" search-input data-status="<?php echo $status; ?>" placeholder="Digite parte do título" style="width: 200px;">
+<div class="search-container">
+    <div>
+        <input type="text" name="search-input" search-input data-status="<?php echo $status; ?>" placeholder="Digite parte do título" style="width: 200px;">
+    </div>
+    <div>
+        <img src="<?php $this->asset('img/spinner_192.gif'); ?>" alt="Carregando..." class="spinner-search d-none" id="spinner-search">
+    </div>
 </div>
