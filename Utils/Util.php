9475810cc1d50c8e9bb50de16ca5ddc4cf7224d3
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-09-23 14:24:18 -0300

    fix: usar variáveis de ambiente do docker-compose do projeto base

--- Utils/Util.php
+++ Utils/Util.php
@@ -14,33 +14,17 @@ class Util
      */
     public static function hasPermission()
     {
-        $app = App::i();
-        $env = self::getEnvironmentVariables();
-
         $query = "SELECT * FROM seal_relation WHERE seal_id = :seal_id AND object_type = :object_type AND object_id = :object_id";
         $params = [
-            "seal_id" => (int) $env["SEAL_ID"],
+            "seal_id" => (int)env('SECULT_SEAL_ID'),
             "object_type" => "MapasCulturais\Entities\Agent",
-            "object_id" => $app->getUser()->profile->id,
+            "object_id" => App::i()->getUser()->profile->id,
         ];
-        $conn = $app->em->getConnection();
+        $conn = App::i()->em->getConnection();
         $result = $conn->fetchAllAssociative($query, $params);
 
         if ($result) return true;
 
         return false;
-    }
-
-    public static function getEnvironmentVariables()
-    {
-        $env_file = PLUGINS_PATH . 'PublicConsultation/.env';
-        $env_file_example = PLUGINS_PATH . 'PublicConsultation/.env.example';
-        $env = parse_ini_file($env_file_example);
-
-        if (file_exists($env_file)) {
-            $env = parse_ini_file($env_file);
-        }
-
-        return $env;
     }
 }
